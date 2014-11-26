8a84a544c47557677520ef82b5b7b3edeff9ae35
Sami Keskinen <sami@stonecrew.fi>
2014-11-26 13:15:34 +0200

    cleanin'

--- src/Glome/ApiBundle/Controller/SecurityController.php
+++ src/Glome/ApiBundle/Controller/SecurityController.php
@@ -1,72 +1,70 @@
 <?php
-namespace Glome\ApiBundle\Controller;
+  namespace Glome\ApiBundle\Controller;
 
-use FOS\RestBundle\Controller\FOSRestController;
-use Glome\ApiBundle\Entity\User;
+  use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+  use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+  use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+  use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 
-use Symfony\Bundle\FrameworkBundle\Controller\Controller;
-use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
-use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
-use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+  use Symfony\Component\HttpFoundation\Request;
+  use Symfony\Component\HttpFoundation\Response;
+  use Symfony\Component\HttpKernel\Exception\HttpException;
+  use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+  use Symfony\Component\Security\Core\SecurityContext;
+  use Symfony\Component\Security\Core\Exception\AccessDeniedException;
+  use Symfony\Component\Security\Core\Authentication\Token\UsernamePasswordToken;
+  use Symfony\Component\Security\Core\Authentication\Token\AnonymousToken;
 
-use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\HttpKernel\Exception\HttpException;
-use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
-use Symfony\Component\Security\Core\Exception\AccessDeniedException;
-use Symfony\Component\Security\Core\Authentication\Token\UsernamePasswordToken;
-use Symfony\Component\Security\Core\Authentication\Token\AnonymousToken;
-
-use FOS\RestBundle\View\RouteRedirectView,
+  use FOS\RestBundle\Controller\FOSRestController,
+    FOS\RestBundle\View\RouteRedirectView,
     FOS\RestBundle\View\View,
     FOS\RestBundle\Controller\Annotations\QueryParam,
     FOS\RestBundle\Controller\Annotations\RequestParam,
     FOS\RestBundle\Request\ParamFetcherInterface;
 
-use Nelmio\ApiDocBundle\Annotation\ApiDoc;
+  use FOS\OAuthServerBundle\FOSOAuthServerBundle;
 
-use GuzzleHttp\Client;
-use FOS\OAuthServerBundle\FOSOAuthServerBundle;
-use Glome\ApiBundle\Entity\GlomeAuthenticationUser;
-use Symfony\Component\Security\Core\SecurityContext;
+  use Nelmio\ApiDocBundle\Annotation\ApiDoc;
 
-class SecurityController extends FOSRestController
-{
-  /**
-   * Default route.
-   *
-   * @Route("/")
-   * @Method({"GET"})
-   *
-   * @return Response
-   */
-  public function indexAction($name)
-    {
-      return $this->render('GlomeApiBundle:Default:index.html.twig', array('name' => $name));
-    }
+  use GuzzleHttp\Client;
 
-  /**
-   * Login.
-   *
-   * @Route("/api/login")
-   * @Method({"GET", "POST"})
-   *
-   * @param Request $request
-   *
-   * @ApiDoc(
-   *  section="System",
-   *  description="Login a User",
-   *  statusCodes={
-   *    200="Success",
-   *    403="Access denied",
-   *    404="User not found"
-   *  }
-   * )
-   *
-   * @return Response
-   */
-  public function loginAction(Request $request)
-    {
+  use Glome\ApiBundle\Entity\User;
+  use Glome\ApiBundle\Entity\GlomeAuthenticationUser;
+
+  class SecurityController extends FOSRestController {
+      /**
+       * Default route.
+       *
+       * @Route("/")
+       * @Method({"GET"})
+       *
+       * @return Response
+       */
+      public function indexAction($name) {
+        return $this->render('GlomeApiBundle:Default:index.html.twig', array('name' => $name));
+      }
+
+      /**
+       * Login.
+       *
+       * @Route("/api/login")
+       * @Method({"GET", "POST"})
+       *
+       * @param Request $request
+       *
+       * @ApiDoc(
+       *  section="System",
+       *  description="Login a User",
+       *  statusCodes={
+       *    200="Success",
+       *    403="Access denied",
+       *    404="User not found"
+       *  }
+       * )
+       *
+       * @return Response
+       */
+      public function loginAction(Request $request) {
         $session = $request->getSession();
 
         if ($request->attributes->has(SecurityContext::AUTHENTICATION_ERROR)) {
@@ -86,49 +84,49 @@ class SecurityController extends FOSRestController
         $lastUsername = (null === $session) ? '' : $session->get(SecurityContext::LAST_USERNAME);
 
         return $this->render(
-            'GlomeApiBundle:Security:login.html.twig',
-            array(
-                'last_username' => $lastUsername,
-                'error' => $error,
-            )
+          'GlomeApiBundle:Security:login.html.twig',
+          array(
+            'last_username' => $lastUsername,
+            'error' => $error,
+          )
         );
-    }
+      }
 
-    public function loginCheckAction(Request $request)
-    {
+      public function loginCheckAction(Request $request) {
+      }
 
-    }
+      /**
+       * Login Glome user.
+       *
+       * @Route("/api/glomelogin", name="/api/glomelogin")
+       * @Method({"GET", "POST"})
+       *
+       * @ApiDoc(
+       *  section="Glome",
+       *  description="Login Glome user",
+       *  statusCodes={
+       *    200="Success",
+       *    403="Access denied",
+       *    404="User not found"
+       *  }
+       * )
+       */
+      public function glomeLoginAction(Request $request) {
+          if ($request->isMethod('POST')) {
+              if ($request->get('reload') != 1) {
+                  $glomeid = $request->request->get('form[glomeid]', 'no glomeid');
+                  $password = $request->request->get('form[password]', 'no password');
 
-    /**
-     * Login Glome user.
-     *
-     * @Route("/api/glomelogin", name="/api/glomelogin")
-     * @Method({"GET","POST"})
-     *
-     * @ApiDoc(
-     *  section="Glome",
-     *  description="Login Glome user",
-     *  statusCodes={
-     *    200="Success",
-     *    403="Access denied",
-     *    404="User not found"
-     *  }
-     * )
-     */
-    public function glomeLoginAction(Request $request)
-    {
-        if ($request->isMethod('POST')) {
-            if ($request->get('reload') != 1) {
-                $glomeid = $request->request->get('form[glomeid]','no glomeid');
-                $password = $request->request->get('form[password]','no password');
-                $lel = $this->forward('GlomeApiBundle:Glome:glomeLogin', array(
-                    'glomeid'  => $glomeid,
-                    'password' => $password,
-                ));
-                return $lel;
-                //return $this->redirect($this->generateUrl('/api/glomeLoginAction', array('glomeid' => $glomeid, 'password' => $password)));
-            }
-        }
+                  $glome_user = $this->forward('GlomeApiBundle:Glome:glomeLogin', array(
+                      'glomeid'  => $glomeid,
+                      'password' => $password,
+                  ));
+                  return $glome_user;
+
+                  // return $this->redirect($this->generateUrl('/api/glomeLoginAction',
+                  //   array('glomeid' => $glomeid, 'password' => $password)));
+              }
+          }
 
         // create a task and give it some dummy data for this example
         $user = new GlomeAuthenticationUser();
@@ -139,9 +137,10 @@ class SecurityController extends FOSRestController
             ->add('save', 'submit', array('label' => 'Login'))
             ->getForm();
 
-        return $this->render('GlomeApiBundle:Glome:index.html.twig', array(
-            'form' => $form->createView(),
-        ));
+        return $this->render(
+          'GlomeApiBundle:Glome:index.html.twig',
+          array('form' => $form->createView())
+        );
 
         /*
         $res = $client->get('https://api.github.com/user', ['auth' =>  ['user', 'pass']]);
@@ -160,46 +159,49 @@ class SecurityController extends FOSRestController
             echo 'I completed! ' . $response;
         });
         */
-    }
+      }
 
-    /**
-     *  @Route("/api/glomeLoginCheck", name="/api/glomeLoginCheck")
-     *  @Method({"GET","POST"})
-     */
-    public function glomeLoginCheckAction(Request $request)
-    {
-        $credentials = $request->request->get('form');
+      /**
+       *  @Route("/api/glomeLoginCheck", name="/api/glomeLoginCheck")
+       *  @Method({"GET", "POST"})
+       */
+      public function glomeLoginCheckAction(Request $request) {
+          $credentials = $request->request->get('form');
 
-        print_r($credentials['glomeid']);
-        $client = new Client();
+          print_r($credentials['glomeid']);
+          $client = new Client();
 
-        $res = $client->post('http://stone.glome.me/users/login.json',
-            ['exceptions' => false, 'body' =>
-                ['user[glomeid]' => $credentials['glomeid'],
-                    'user[password]' => $credentials['password']]]);
+          $res = $client->post('http://stone.glome.me/users/login.json',
+            [
+              'exceptions' => false,
+              'body' => [
+                'user[glomeid]' => $credentials['glomeid'],
+                'user[password]' => $credentials['password']
+              ]
+            ]);
 
-        if ($res->getStatusCode() != 200) {
-            return $this->redirect($this->generateUrl('/api/login', array('reload' => 1)));
-        }
+          if ($res->getStatusCode() != 200) {
+              return $this->redirect($this->generateUrl('/api/login', array('reload' => 1)));
+          }
 
-        echo var_export($res->json());
+          echo var_export($res->json());
 
-        /*
-        $res = $client->get('https://api.github.com/user', ['auth' =>  ['user', 'pass']]);
-        echo $res->getStatusCode();
-        // "200"
-        echo $res->getHeader('content-type');
-        // 'application/json; charset=utf8'
-        echo $res->getBody();
-        // {"type":"User"...'
-        var_export($res->json());
-        // Outputs the JSON decoded data
+          /*
+          $res = $client->get('https://api.github.com/user', ['auth' =>  ['user', 'pass']]);
+          echo $res->getStatusCode();
+          // "200"
+          echo $res->getHeader('content-type');
+          // 'application/json; charset=utf8'
+          echo $res->getBody();
+          // {"type":"User"...'
+          var_export($res->json());
+          // Outputs the JSON decoded data
 
-        // Send an asynchronous request.
-        $req = $client->createRequest('GET', 'http://httpbin.org', ['future' => true]);
-        $client->send($req)->then(function ($response) {
-            echo 'I completed! ' . $response;
-        });
-        */
-    }
-}
+          // Send an asynchronous request.
+          $req = $client->createRequest('GET', 'http://httpbin.org', ['future' => true]);
+          $client->send($req)->then(function ($response) {
+              echo 'I completed! ' . $response;
+          });
+          */
+      }
+  }
